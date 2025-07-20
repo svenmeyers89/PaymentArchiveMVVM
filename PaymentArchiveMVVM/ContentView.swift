dbab9cf94ba9825ce8d3a37e81f3b399a315121f
Sven Majeric <svenmajeric@Mac.fritz.box>
2025-07-20 16:09:26 +0200

    Adds project

---
+++ PaymentArchiveMVVM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaymentArchiveMVVM
+//
+//  Created by Sven Majeric on 20.07.2025..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaymentArchiveMVVM/PaymentArchiveMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaymentArchiveMVVMApp.swift
+//  PaymentArchiveMVVM
+//
+//  Created by Sven Majeric on 20.07.2025..
+//
+
+import SwiftUI
+
+@main
+struct PaymentArchiveMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
