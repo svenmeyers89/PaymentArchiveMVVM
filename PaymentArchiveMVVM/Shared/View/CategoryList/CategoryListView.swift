6bfeb1d2d2d6cb0ea68b314c7a026312205d502a
Sven <smajerich@gmail.com>
2026-01-20 22:43:11 +0100

    Implements CategoryListView

---
+++ PaymentArchiveMVVM/Shared/View/CategoryList/CategoryListView.swift
@@ -0,0 +1,98 @@
+//
+//  CategoryListView.swift
+//  PaymentArchiveMVVM
+//
+//  Created by Sven Majeric on 20.01.2026..
+//
+
+import Foundation
+import SwiftUI
+
+struct CategoryListView: View {
+  struct Colors {
+    let categoryIcon: CategoryIcon.Colors
+    let categoryListItem: CategoryListItemView.Colors
+    let background: Color
+  }
+  
+  @State private var selectedPaymentCategories: Set<Payment.Category>
+  
+  private let allPaymentCategories: [Payment.Category]
+  private let colors: Colors
+  private let onSave: (Set<Payment.Category>) -> Void
+
+  init(
+    allPaymentCategories: [Payment.Category],
+    selectedPaymentCategories: Set<Payment.Category>,
+    colors: Colors,
+    onSave: @escaping (Set<Payment.Category>) -> Void
+  ) {
+    self._selectedPaymentCategories = State(initialValue: selectedPaymentCategories)
+    self.allPaymentCategories = allPaymentCategories
+    self.colors = colors
+    self.onSave = onSave
+  }
+  
+  var body: some View {
+    List {
+      Section {
+        CategoryListItemView(
+          itemType: .allCategories,
+          colors: colors.categoryListItem,
+          isSelected: Binding<Bool>(
+            get: { selectedPaymentCategories == Set<Payment.Category>(allPaymentCategories) },
+            set: { isSelected in
+              if isSelected {
+                selectedPaymentCategories = Set<Payment.Category>(allPaymentCategories)
+              } else {
+                selectedPaymentCategories.removeAll()
+              }
+            }
+          ))
+      }
+      
+      Section {
+        ForEach(allPaymentCategories, id: \.rawValue) { paymentCategory in
+          CategoryListItemView(
+            itemType: .paymentCategory(paymentCategory, categoryIconColors: colors.categoryIcon),
+            colors: colors.categoryListItem,
+            isSelected: Binding<Bool>(
+              get: { selectedPaymentCategories.contains(paymentCategory) },
+              set: { isSelected in
+                if isSelected {
+                  selectedPaymentCategories.insert(paymentCategory)
+                } else {
+                  selectedPaymentCategories.remove(paymentCategory)
+                }
+              }
+            )
+          )
+        }
+      }
+    }
+    .toolbar {
+      ToolbarItem(placement: .confirmationAction) {
+        Button("Save") {
+          onSave(selectedPaymentCategories)
+        }
+      }
+    }
+    .background(colors.background)
+  }
+}
+
+#Preview {
+  NavigationStack {
+    CategoryListView(
+      allPaymentCategories: [.accommodation, .groceries, .presents, .restaurantsAndBars],
+      selectedPaymentCategories: Set<Payment.Category>([.accommodation]),
+      colors: .init(
+        categoryIcon: .init(iconBackground: .green, iconTint: .white),
+        categoryListItem: .init(title: .black, background: .white, toggle: .blue),
+        background: .white
+      )
+    ) { selectedPaymentCategories in
+      print("Selected categories: \(selectedPaymentCategories)")
+    }
+  }
+}
